20d1e350a53a4e718edcb98b755e0cc694e75bf4
arrkiin <arrkiin@gmail.com>
2018-02-21 15:55:12 +0100

    Working on user authentication via firebase

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,12 @@
+const admin = require('firebase-admin');
+
 module.exports = (req, res) => {
-    res.send(req.body);
+    // Verify the user provide a phone
+    if (!req.body.phone) {
+        return res.status(422).send({ error: 'Bad Input' });
+    }
+    // Format the phone number to remove dashes and parens
+    const phone = String(req.body.phone).replace(/[^\d]/g, '');
+    // Create new user account using that phone number
+    // Respond to the user request, say the account was made
 };
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
+const serviceAccount = require('./one-time-password.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
