a763847d8c0e4f64ebbf8f51f933383d5266dc2b
arrkiin <arrkiin@gmail.com>
2018-02-21 15:39:57 +0100

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const createUser = require('./create_user');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://one-time-password-3fa6b.firebaseio.com',
+});
+
+exports.createUser = functions.https.onRequest(createUser);
